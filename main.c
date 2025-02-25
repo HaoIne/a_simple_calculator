f7e1a7e66a272382be025045188dfdd0f525a3b4
HaoIne <18970089974@163.com>
2025-02-25 23:43:44 +0800

    an_unfinished_calculator

--- main.c
+++ main.c
@@ -3,10 +3,7 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define MAX_L_EX 100
-
-int l_infix_ex=0,
-    l_rpn=0;
+#define MAX_L_EX 101
 
 // Structure definition
 typedef struct {
@@ -22,8 +19,28 @@ typedef struct {
     } value;
 } Expression_elements;
 
+typedef enum {
+    TOKEN_NUMBER,
+    TOKEN_PLUS,
+    TOKEN_DIVIDE,
+    TOKEN_MULTIPLY,
+    TOKEN_MINUS,
+    TOKEN_LPAREN,
+    TOKEN_RPAREN,
+    TOKEN_END,
+    TOKEN_INVALID
+} Token_type;
+
+typedef struct {
+    Token_type type;
+} Token;
+
 // Function prototypes
 void get_infix_ex(Expression_elements *infix_ex);
+Token get_token(Expression_elements infix_ex);
+bool parse_expression(const Expression_elements *infix_ex, int *index_infix_ex);
+bool parse_term(const Expression_elements *infix_ex, int *index_infix_ex);
+bool parse_factor(const Expression_elements *infix_ex, int *index_infix_ex);
 void turn_to_rpn(const Expression_elements *infix_ex, Expression_elements *rpn);
 void stack_init(Stack *s);
 bool is_empty(const Stack *s);
@@ -73,6 +90,10 @@ void get_infix_ex(Expression_elements *infix_ex) {
             exit(1);
         }
 
+        if(ch==' ') {
+            continue;
+        }
+
         if(is_digit(ch)) {
             if(last_is_digit) {
                 last_num=last_num*10+ch-'0';
@@ -101,16 +122,90 @@ void get_infix_ex(Expression_elements *infix_ex) {
         }
     }
 
-    l_infix_ex=index_infix_ex;
+    // Add '\0' as the end. 
+    infix_ex[index_infix_ex].is_num=false;
+    infix_ex[index_infix_ex++].value.op='\0';
 
     // Debugging expressions
-    for(int i=0;i<l_infix_ex;i++) {
+    for(int i=0 ; infix_ex[index_infix_ex].is_num && infix_ex[index_infix_ex].value.op=='\0' ; i++) {
         if(infix_ex[i].is_num) {
             printf("%.0f",infix_ex[i].value.num);
         } else {
             printf("%c",infix_ex[i].value.op);
         }
     } printf("\n");
+}
+
+Token get_token(const Expression_elements infix_ex) {
+    // This function is used to get a char from infix expression and turn it into a token.(working as a Lexer)
+    // @param infix_ex: an element of the infix expression struct array.
+    // @return token: the token wo get from infix expression.
+    if(infix_ex.is_num) {
+        return (Token){TOKEN_NUMBER};
+    } else {
+        Token token;
+        switch(infix_ex.value.op) {
+            case '+': token.type=TOKEN_PLUS; break;
+            case '-': token.type=TOKEN_MINUS; break;
+            case '*': token.type=TOKEN_MULTIPLY; break;
+            case '/': token.type=TOKEN_DIVIDE; break;
+            case '\0': token.type=TOKEN_END; break;
+            // Process the invalid token.
+            default: token.type=TOKEN_INVALID;
+                     fprintf(stderr, "ERROR: Invalid token %c", token.type);
+                     exit(1);
+        }
+        return token;
+    }
+}
+
+// TODO: Yesterday the last thing I did is adding '\0' to the tail of expressions as the end, then using this '\0' to determine if the expression has ended.
+// The next thing I should do is complete the parse. (Note that the token_type enum contains sth I haven't use, like TOKEN_END)
+// And notice the ending condition of parse.
+bool parse_expression(const Expression_elements *infix_ex, int *index_infix_ex) {
+    if(!parse_term(infix_ex, index_infix_ex)) {
+        fprintf(stderr, "ERROR: Expected term.");
+        exit(1);
+    }
+    Token token=get_token(infix_ex[*index_infix_ex]); (*index_infix_ex)++;
+    while(token.type==TOKEN_PLUS || token.type==TOKEN_MINUS) {
+        if(!parse_term(infix_ex, index_infix_ex)) {
+            fprintf(stderr, "ERROR: Expected term.");
+            exit(1);
+        }
+        token=get_token(infix_ex[*index_infix_ex]); (*index_infix_ex)++;
+    }
+    (*index_infix_ex)--;
+    return true;
+}
+
+bool parse_term(const Expression_elements *infix_ex, int *index_infix_ex) {
+    if(!parse_factor(infix_ex, index_infix_ex)) {
+        fprintf(stderr, "ERROR: Expected factor.");
+        exit(1);
+    }
+    Token token=get_token(infix_ex[*index_infix_ex]); (*index_infix_ex)++;
+    while(token.type==TOKEN_MULTIPLY || token.type==TOKEN_DIVIDE) {
+        if(!parse_factor(infix_ex, index_infix_ex)) return false;
+        token=get_token(infix_ex[*index_infix_ex]); (*index_infix_ex)++;
+    }
+    (*index_infix_ex)--;
+    return true;
+}
+
+bool parse_factor(const Expression_elements *infix_ex, int *index_infix_ex) {
+    Token token=get_token(infix_ex[*index_infix_ex]); (*index_infix_ex)++;
+    if(token.type==TOKEN_LPAREN) {
+        if(!parse_expression(infix_ex, index_infix_ex)) return false;
+        token=get_token(infix_ex[*index_infix_ex]); (*index_infix_ex)++;
+        if(token.type!=TOKEN_RPAREN) {
+            fprintf(stderr, "ERROR: Expected ')'.");
+            exit(1);
+        }
+    } else if(token.type!=TOKEN_NUMBER) {
+        return false;
+    }
+    return true;
 }
 
 // Stack operation functions
@@ -189,18 +284,20 @@ void turn_to_rpn(const Expression_elements *infix_ex, Expression_elements *rpn) 
 
     Stack stack;
     stack_init(&stack);
-    
-    int index_infix_ex=0;
+
     int index_rpn=0;
 
     // Using scheduling algorithm to turn the infix expression to reversed poland notation.
-    for(index_infix_ex=0;index_infix_ex<l_infix_ex;index_infix_ex++) {
+    for (int index_infix_ex=0;;index_infix_ex++) {
         // Process the numbers.
         if(infix_ex[index_infix_ex].is_num) {
             rpn[index_rpn].is_num=true;
             rpn[index_rpn++].value.num=infix_ex[index_infix_ex].value.num;
             printf("%.0f ",rpn[index_rpn-1].value.num);
         } else {
+            // The ending condition.
+            if(infix_ex[index_infix_ex].value.op=='\0') break;
+
             // Process the operators.
             if(is_empty(&stack) || check_top(&stack)=='(' || infix_ex[index_infix_ex].value.op=='(') {
                 push(&stack, infix_ex[index_infix_ex].value.op);
@@ -224,6 +321,7 @@ void turn_to_rpn(const Expression_elements *infix_ex, Expression_elements *rpn) 
         }
     }
 
+    // Clear the stack (pop out the remaining operators in the stack).
     while(!is_empty(&stack)) {
         rpn[index_rpn].is_num=false;
         rpn[index_rpn++].value.op=(char)pop(&stack);
@@ -231,6 +329,10 @@ void turn_to_rpn(const Expression_elements *infix_ex, Expression_elements *rpn) 
     }
 
     printf("\n");
+
+    // Add '\0' as the end.
+    rpn[index_rpn].is_num=false;
+    rpn[index_rpn++].value.op='\0';
 
     // For debugging
     for(int i=0;i<index_rpn;i++) {
@@ -241,7 +343,6 @@ void turn_to_rpn(const Expression_elements *infix_ex, Expression_elements *rpn) 
         }
     }
     printf("\n");
-    l_rpn=index_rpn;
 }
 
 double compute_rpn(const Expression_elements *rpn) {
@@ -254,9 +355,14 @@ double compute_rpn(const Expression_elements *rpn) {
     Stack stack;
     stack_init(&stack);
 
-    for(int index_rpn = 0;index_rpn<l_rpn;index_rpn++) {
+    for(int index_rpn = 0;;index_rpn++) {
         if(rpn[index_rpn].is_num) {
             push(&stack, rpn[index_rpn].value.num);
+
+        // Ending condition.
+        } else if(rpn[index_rpn].value.op=='\0') {
+            break;
+
         } else if(!is_empty(&stack)) {
             push(&stack, apply_operators(pop(&stack), pop(&stack), rpn[index_rpn].value.op));
         }
